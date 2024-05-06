9696c18d7fc32dab096a17d94be602391dfeadc8
alcuincrow <casalcuin.crow@gmail.com>
2024-05-05 21:59:02 -0400

    added themetoggle, updated bio

---
+++ main.js
@@ -0,0 +1,9 @@
+//Toggle light/dark theme
+let darkLight = document.getElementById("themeToggle");
+darkLight.addEventListener('click', function themeToggle() {
+    let element = document.body;
+    element.classList.toggle("dark-mode")
+}
+
+)
+
